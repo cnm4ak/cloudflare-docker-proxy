622e37c64d3915c2bae4e177ebf26ef6ea7e9e9c
cnm4ak <cn.m4ak@gmail.com>
2024-06-18 16:30:24 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.itif.fun": dockerHub,
+  "quay.itif.fun": "https://quay.io",
+  "gcr.itif.fun": "https://gcr.io",
+  "k8s-gcr.itif.fun": "https://k8s.gcr.io",
+  "k8s.itif.fun": "https://registry.k8s.io",
+  "ghcr.itif.fun": "https://ghcr.io",
+  "cloudsmith.itif.fun": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.itif.fun": dockerHub,
 };
 
 function routeByHosts(host) {
